262d30c8f84abe5527a9642ef2c728eed83e49be
Austin Ferguson <ferguson.austin.j@gmail.com>
2025-03-16 21:47:54 -0400

    Add single gradient experiments

---
+++ experiments/single_gradient/gradient_50/scrape_all_data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+this_dir=$(pwd)
+for dir in $(ls | grep 2025_)
+do
+    echo "$dir"
+    if [ ! $dir = "2025_03_16_01__well_mixed" ]
+    then
+        cp 2025_03_16_01__well_mixed/data/scripts/* ${dir}/data/scripts
+    else
+        continue
+    fi
+    cd $dir/data/scripts
+    ./scrape_phylogenetic_data.sh  
+    ./scrape_ifg_discoveries.sh    
+    ./scrape_sweep_data.sh
+    cd $this_dir
+done
