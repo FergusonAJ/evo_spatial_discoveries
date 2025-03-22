6fd5ab1e0f7d5cb53fd10c2438edf554b034025f
Austin Ferguson <ferguson.austin.j@gmail.com>
2025-03-22 11:00:09 -0400

    Add analyses for first batch of multipath

--- experiments/multipath_fan/first_batch__gradient_0_01/2025_03_13_x1__combined_analysis/analysis/01_plot_phylo_data.R
+++ experiments/multipath_fan/first_batch__gradient_0_01/2025_03_13_x1__combined_analysis/analysis/01_plot_phylo_data.R
@@ -2,25 +2,47 @@ rm(list = ls())
 
 library(ggplot2)
 
-df_phylo = read.csv('../data/combined_phylo_data.csv')
+plot_dir = '../plots/phylo'
+if(!dir.exists(plot_dir)){
+  dir.create(plot_dir, recursive = T)
+}
+
 max_gen = 100000
+
+df_phylo = read.csv('../data/combined_phylo_data.csv')
+
+df_phylo$structure_str = gsub('_', ' ', df_phylo$structure)
 df_final = df_phylo[df_phylo$Generation == max_gen,]
 
 ggplot(df_phylo, aes(x = Generation, y = phylogenetic_diversity, group = rep)) + 
   geom_line(alpha=0.1) + 
-  facet_grid(rows=vars(as.factor(structure)), cols = vars(as.factor(gradient)))
+  ylab('Phylogenetic diversity') + 
+  xlab('Generation') +
+  facet_wrap(vars(as.factor(structure_str)))
+ggsave(paste0(plot_dir, '/phylo_diversity_over_time.pdf'), units = 'in', width = 10, height = 8)
 
 ggplot(df_phylo, aes(x = Generation, y = mean_pairwise_distance, group = rep)) + 
   geom_line(alpha=0.1) + 
-  facet_grid(rows=vars(as.factor(structure)), cols = vars(as.factor(gradient)))
+  ylab('Mean pairwise distance') + 
+  xlab('Generation') +
+  facet_wrap(vars(as.factor(structure_str)))
+ggsave(paste0(plot_dir, '/mean_pairwise_dist_over_time.pdf'), units = 'in', width = 10, height = 8)
 
 ggplot(df_final, aes(x = as.factor(structure), y = phylogenetic_diversity)) + 
   geom_boxplot() + 
   geom_jitter() + 
-  facet_grid(cols = vars(as.factor(gradient)))
+  ylab('Final phylogenetic diversity') + 
+  xlab('Spatial structure') + 
+  theme(axis.text.x = element_text(angle=45, hjust = 1)) + 
+  scale_y_continuous(limits = c(0,NA))
+ggsave(paste0(plot_dir, '/phylo_diversity_final.pdf'), units = 'in', width = 6, height = 5)
   
-
 ggplot(df_final, aes(x = as.factor(structure), y = mean_pairwise_distance)) + 
   geom_boxplot() + 
-  geom_jitter() +
-  facet_grid(cols = vars(as.factor(gradient)))
+  geom_jitter() + 
+  ylab('Final mean pairwise distance') + 
+  xlab('Spatial structure') + 
+  scale_y_continuous(limits = c(0,NA)) +
+  theme(axis.text.x = element_text(angle=45, hjust = 1))
+ggsave(paste0(plot_dir, '/mean_pairwise_dist_final.pdf'), units = 'in', width = 6, height = 5)
+
--- experiments/multipath_fan/first_batch__gradient_0_01/2025_03_13_x1__combined_analysis/analysis/03_plot_ifg_data.R
+++ experiments/multipath_fan/first_batch__gradient_0_01/2025_03_13_x1__combined_analysis/analysis/03_plot_ifg_data.R
@@ -2,21 +2,55 @@ rm(list = ls())
 
 library(ggplot2)
 
+plot_dir = '../plots/ifg'
+if(!dir.exists(plot_dir)){
+  dir.create(plot_dir, recursive = T)
+}
+
 df_ifg = read.csv('../data/combined_ifg_data.csv')
 
-ggplot(df_ifg, aes(x = as.factor(node), y = steps)) + 
-  geom_boxplot(outlier.shape = NA) + 
-  geom_jitter() + 
-  scale_y_log10() +
-  facet_grid(rows = vars(as.factor(structure)), cols=vars(as.factor(gradient)))
+df_ifg$structure_str = gsub('_', ' ', df_ifg$structure)
 
 ggplot(df_ifg, aes(x = as.factor(node), y = steps)) + 
   geom_boxplot(outlier.shape = NA) + 
   geom_jitter() + 
-  scale_y_log10() +
-  facet_grid(cols = vars(as.factor(structure)), rows=vars(as.factor(gradient)))
+  xlab('Gradient steepness') + 
+  ylab('Maximum step reached (max = 1000)') +
+  facet_grid(cols = vars(as.factor(structure_str)))
+ggsave(paste0(plot_dir, '/ifg_steps_per_gradient.pdf'), units = 'in', width = 12, height = 6)
 
 ggplot(df_ifg, aes(x = as.factor(node), y = steps)) + 
   geom_boxplot(outlier.shape = NA) + 
   geom_jitter() + 
-  facet_grid(cols = vars(as.factor(structure)), rows=vars(as.factor(gradient)))
+  xlab('Gradient steepness') + 
+  ylab('Maximum step reached (max = 1000)') +
+  facet_grid(cols = vars(as.factor(structure_str))) + 
+  scale_y_log10() 
+ggsave(paste0(plot_dir, '/ifg_steps_per_gradient_log_y.pdf'), units = 'in', width = 12, height = 6)
+
+ggplot(df_ifg, aes(x = as.factor(structure_str), y = steps)) + 
+  geom_boxplot(outlier.shape = NA) + 
+  geom_jitter() + 
+  xlab('Spatial structure') + 
+  ylab('Maximum step reached (max = 1000)') +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  facet_grid(rows=vars(as.factor(node)))
+ggsave(paste0(plot_dir, '/ifg_steps_per_gradient_faceted.pdf'), units = 'in', width = 6, height = 12)
+
+ggplot(df_ifg[df_ifg$node == 5,], aes(x = as.factor(structure_str), y = steps)) + 
+  geom_boxplot(outlier.shape = NA) + 
+  geom_jitter() + 
+  xlab('Spatial structure') + 
+  ylab('Maximum step reached (max = 1000)') +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  facet_grid(rows=vars(as.factor(node)))
+ggsave(paste0(plot_dir, '/ifg_steps_gradient_5.pdf'), units = 'in', width = 6, height = 4)
+
+ggplot(df_ifg[df_ifg$node == 4,], aes(x = as.factor(structure_str), y = steps)) + 
+  geom_boxplot(outlier.shape = NA) + 
+  geom_jitter() + 
+  xlab('Spatial structure') + 
+  ylab('Maximum step reached (max = 1000)') +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  facet_grid(rows=vars(as.factor(node)))
+ggsave(paste0(plot_dir, '/ifg_steps_gradient_4.pdf'), units = 'in', width = 6, height = 4)
