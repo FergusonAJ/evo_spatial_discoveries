b93074bcd0e4c2324e790600b96583c85fca6403
Austin Ferguson <ferguson.austin.j@gmail.com>
2025-04-01 10:21:41 -0400

    Add squished toroid single grad and multipath exps

---
+++ experiments/toroids_single_gradient/gradient_50/scrape_all_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+this_dir=$(pwd)
+for dir in $(ls | grep 2025_)
+do
+    echo "$dir"
+    if [ ! $dir = "2025_04_01_01__60_60" ]
+    then
+        cp 2025_04_01_01__60_60/data/scripts/* ${dir}/data/scripts
+    fi
+    cd $dir/data/scripts
+    ./scrape_phylogenetic_data.sh  
+    ./scrape_ifg_discoveries.sh    
+    ./scrape_sweep_data.sh
+    cd $this_dir
+done
