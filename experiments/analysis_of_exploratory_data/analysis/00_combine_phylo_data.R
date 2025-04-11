a8f820ff63916ebd522678468e05cf3ebf65f267
Austin Ferguson <ferguson.austin.j@gmail.com>
2025-04-11 16:59:46 -0400

    Add combined analysis of initial exploratory data

---
+++ experiments/analysis_of_exploratory_data/analysis/00_combine_phylo_data.R
@@ -0,0 +1,37 @@
+rm(list = ls())
+
+exp_paths = c(
+  '../../single_gradient/gradient_50',
+  '../../multipath_fan/varied_lengths',
+  '../../valley_crossing/'
+)
+exp_names = c(
+  'Single gradient', 
+  'Multipath', 
+  'Valley crossing'
+)
+
+output_dir = '../data'
+if(!dir.exists(output_dir)) dir.create(output_dir, recursive = T)
+
+df_combined = NA
+for(exp_id in 1:length(exp_paths)){
+  exp_path = exp_paths[exp_id]
+  exp_name = exp_names[exp_id]
+  cat('Processing:', exp_name, '\n')
+  dirs = list.dirs(exp_path, recursive = F)
+  exp_analysis_dir = dirs[grepl('_x1_', dirs)]
+  phylo_filename = paste0(exp_analysis_dir, '/data/combined_phylo_data.csv')
+  df_exp_phylo = read.csv(phylo_filename)
+  df_exp_phylo$landscape = exp_name
+  if('X' %in% colnames(df_exp_phylo)){
+    df_exp_phylo = df_exp_phylo[,setdiff(colnames(df_exp_phylo), 'X')]
+  }
+  if(!is.data.frame(df_combined)) df_combined = df_exp_phylo
+  else df_combined = rbind(df_combined, df_exp_phylo)
+}
+
+df_combined$structure_str = gsub('_', ' ', df_combined$structure)
+
+output_filename = paste0(output_dir, '/combined_phylo_data.csv')
+write.csv(df_combined, output_filename, row.names = F)
