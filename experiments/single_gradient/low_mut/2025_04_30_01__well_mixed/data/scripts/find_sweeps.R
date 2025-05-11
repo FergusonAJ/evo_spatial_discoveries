11748d8e0625a3412213491e836c1b49c618de20
Austin Ferguson <ferguson.austin.j@gmail.com>
2025-05-11 14:42:42 -0400

    Remove low mut data

--- experiments/single_gradient/low_mut/2025_04_30_01__well_mixed/data/scripts/find_sweeps.R
+++
@@ -1,7 +0,0 @@
-rm(list = ls())
-
-df = read.csv('phylogenetic_data.csv')
-df$prev_mrca_depth = c(-1, df$mrca_depth[1:(nrow(df) - 1)])
-df_sweeps = df[df$mrca_depth != df$prev_mrca_depth,]
-write.csv(df_sweeps, 'sweeps.csv', row.names=F)
-
